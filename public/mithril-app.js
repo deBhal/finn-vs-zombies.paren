047257cc05f735b7bb231f68d0903e806f2109d0
Julian de Bhal <julian.debhal@automattic.com>
2018-01-21 12:22:42 +1000

    Build

--- public/mithril-app.js
+++ public/mithril-app.js
@@ -29,26 +29,46 @@ function mzombie(props) {
                     });
 };
 function peashooter(props) {
+    if (props === undefined) {
+        props = { 'x' : 250 };
+    };
     return m('img', { 'key' : 'pea',
                       'src' : './assets/pea-shooter-pixelated.png',
                       'onclick' : function (event) {
-        window.spawnShotbang();
+        plantFoodbang(props);
         return event['stopPropagation']();
     },
-                      'style' : { 'position' : 'absolute', 'top' : '25%' }
+                      'style' : { 'position' : 'absolute',
+                                  'left' : px(props.x),
+                                  'top' : '25%'
+                                }
                     });
 };
 objValues = R.values;
 mapObjIndexed = R.mapObjIndexed;
 function mapp() {
     return m('div', { 'id' : 'mapp', 'position' : 'relative' }, m('div', { 'class-name' : 'row',
-                                                                           'onclick' : STATE.callbacks['create-zombie'],
+                                                                           'onclick' : createZombiebang,
                                                                            'style' : { 'position' : 'relative',
                                                                                        'height' : '140px',
                                                                                        'background-color' : '#afa'
                                                                                      }
-                                                                         }, peashooter(), mapcar(mzombie, objValues(STATE.zombies)), mapcar(shot, shots())), m('button', { 'onclick' : playPause }, STATE.running ? 'Pause' : 'Play'), !STATE.running && m('button', { 'onclick' : step }, 'step'), m('button', { 'onclick' : window.createZombiebang }, 'Spawn Zombie'), m('button', { 'onclick' : window.initStatebang }, 'Reset'), m('pre', { 'style' : { 'position' : 'relative' } }, JSON.stringify({ 'state' : STATE }, null, 2)));
+                                                                         }, mapcar(peashooter, objValues(STATE.plants)), mapcar(mzombie, objValues(STATE.zombies)), mapcar(shot, shots())), m('button', { 'onclick' : playPause }, STATE.running ? 'Pause' : 'Play'), !STATE.running && m('button', { 'onclick' : step }, 'step'), m('button', { 'onclick' : window.createZombiebang }, 'Spawn Zombie'), m('button', { 'onclick' : window.spawnPlantbang }, 'Spawn Plant'), m('button', { 'onclick' : window.initStatebang }, 'Reset'), m('pre', { 'style' : { 'position' : 'relative' } }, JSON.stringify({ 'state' : STATE }, null, 2)));
 };
 function mrender() {
     return m.render(mcontainer, mapp());
+};
+initStatebang();
+tickbang();
+function mainLoop() {
+    try {
+        return STATE.running && step();
+    } finally {
+        requestAnimationFrame(mainLoop);
+    };
+};
+mainLoop();
+function step() {
+    tickbang();
+    return mrender();
 };
--- public/state.js
+++ public/state.js
@@ -1,16 +1,66 @@
 console.log('state.paren: Trident is working as expected');
+assocPath = R.assocPath;
+clone = R.clone;
+dropLast = R.dropLast;
+filter = R.filter;
+head = R.head;
+isEmpty = R.isEmpty;
+keys = R.keys;
+last = R.last;
 mapcar = R.map;
-function generateNewZombieIndex() {
-    return STATE.zombieIndex = 1 + STATE.zombieIndex;
+mergeAll = R.mergeAll;
+minBy = R.minBy;
+path = R.path;
+prop = R.prop;
+reduce = R.reduce;
+reject = R.reject;
+tail = R.tail;
+take = R.take;
+objValues = R.values;
+function merge() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return reduce(R.merge, {  }, args);
 };
 var initialState = { 'title' : 'Finn vs Zombies',
                      'running' : true,
                      'time' : 0,
-                     'zombieIndex' : 0,
+                     'zombieIndex' : 1,
                      'zombies' : {  },
+                     'plants' : {  },
                      'shots' : {  }
                    };
-var SHOTSPEED = 10;
+function get() {
+    var propPath = Array.prototype.slice.call(arguments, 0);
+    return path(propPath)(STATE);
+};
+function generateNewMobIndex() {
+    return STATE.zombieIndex = 1 + STATE.zombieIndex;
+};
+/** Change the state */
+function updatebang() {
+    var pathAndValue = Array.prototype.slice.call(arguments, 0);
+    var value = last(pathAndValue);
+    var path = dropLast(1, pathAndValue);
+    STATE = assocPath(path, value, STATE);
+    return STATE;
+};
+function constant(key) {
+    return STATE['constants'][key];
+};
+function tempProp(key, duration) {
+    var result = {  };
+    result[key] = merge({ 'at' : time() }, { 'duration' : 'undefined' !== typeof duration ? duration : defaultDuration(key) });
+    return result;
+};
+function isExpiredTempProp(obj) {
+    return obj && obj.at + obj.duration <= time();
+};
+function filterOutExpiredTempProps(obj) {
+    return reject(isExpiredTempProp, obj);
+};
+function defaultDuration(key) {
+    return get('defaultDurations', key) || get('defaultDurations', 'default');
+};
 function randomintbelow(n) {
     return Math.floor(n * Math.random());
 };
@@ -21,9 +71,9 @@ function createZombiebang(x) {
     if (x === undefined) {
         x = 1200;
     };
-    var x21 = Number.isFinite(x) ? x : 1200;
-    var index = generateNewZombieIndex();
-    var zombie = { 'x' : x21,
+    var x223 = Number.isFinite(x) ? x : 1200;
+    var index = generateNewMobIndex();
+    var zombie = { 'x' : x223,
                    'y' : plusorminus(5),
                    'health' : 10,
                    'index' : index
@@ -31,20 +81,37 @@ function createZombiebang(x) {
     STATE['zombies'][index] = zombie;
     return zombie;
 };
-function spawnShotbang(plantId) {
-    if (plantId === undefined) {
-        plantId = 0;
+function spawnShotbang(plant) {
+    if (plant === undefined) {
+        plant = { 'x' : 120, 'index' : 0 };
     };
-    var index = generateNewZombieIndex();
-    var x = 120;
+    var index = generateNewMobIndex();
+    var plantX = plant && plant.x || 0;
+    var x = plantX + 60;
     return STATE['shots'][index] = { x : x,
                                   yJiggle : plusorminus(5),
-                                  shooterId : plantId,
+                                  shooterId : index,
                                   index : index
                                 };
 };
-function initZombie() {
-    return null;
+function spawnPlantbang() {
+    var _js225 = arguments.length;
+    for (var n224 = 0; n224 < _js225; n224 += 2) {
+        switch (arguments[n224]) {
+        case 'x':
+            x = arguments[n224 + 1];
+        };
+    };
+    var x;
+    var x226 = x || 70 * keys(get('plants')).length;
+    var index = generateNewMobIndex();
+    return STATE['plants'][index] = merge(defaultPlant, { x : x226, index : index });
+};
+function time() {
+    return STATE.time;
+};
+function plantFoodbang(plant) {
+    return updatebang('plants', plant.index, merge(plant, tempProp('plantFooded')));
 };
 function initZombies() {
     return mapcar(createZombiebang, [900]);
@@ -59,64 +126,92 @@ function walk(zombie) {
 function resetZombie(zombie) {
     return Object.assign(zombie, { 'x' : 1200, 'health' : 10 });
 };
-function createShot(plant) {
-    var x = 70;
-    var y = 55;
-    return { 'x' : x, 'y' : y };
+defaultPlant = { x : 0, index : 0 };
+function plantFoodedShotCooldown() {
+    return defaultDuration('plantFooded') / constant('plantFoodedShotCount');
+};
+function shotIsCool(plant) {
+    var object227 = merge(defaultPlant, plant);
+    var currentTime = STATE.time;
+    var timeSinceLastShot = currentTime - object227.timeOfLastShot;
+    var cooldown = plantFoodActivep(plant) ? plantFoodedShotCooldown() : constant('plantShotCooldown');
+    return !object227.timeOfLastShot || cooldown <= timeSinceLastShot;
+};
+function plantFoodActivep(plant) {
+    return plant.plantFooded;
+};
+function updatePlant(plant) {
+    var plant228 = filterOutExpiredTempProps(plant);
+    var object229 = merge(defaultPlant, plant228);
+    var currentTime = STATE.time;
+    var timeSinceLastShot = currentTime - object229.timeOfLastShot;
+    var hasTarget = !isEmpty(STATE.zombies);
+    var wantsToShoot = hasTarget || object229.plantFooded;
+    var shouldShoot = wantsToShoot && shotIsCool(plant228);
+    return mergeAll([filterOutExpiredTempProps(plant228), { derived : { shouldShoot : shouldShoot,
+                                                                        timeSinceLastShot : timeSinceLastShot,
+                                                                        currentTime : currentTime,
+                                                                        wantsToShoot : wantsToShoot,
+                                                                        hasTarget : hasTarget
+                                                                      } }, shouldShoot && (spawnShotbang(plant228), { timeOfLastShot : currentTime })]);
 };
 function updateShot(aShot) {
-    aShot.x = SHOTSPEED + aShot.x;
+    aShot.x = constant('shotSpeed') + aShot.x;
     return aShot;
-};
-function resetShot() {
-    return aShot = createShot();
 };
 function updateZombieHitByShot() {
     return aZombie.health -= 1;
 };
-clone = R.clone;
 function initStatebang() {
     STATE = clone(initialState);
-    initZombie();
     initZombies();
     STATE.callbacks = { 'create-zombie' : function () {
         return createZombiebang();
     } };
+    updatebang('constants', { 'plantShotCooldown' : 150,
+                              'plantFoodedShotCount' : 50,
+                              shotSpeed : 4
+                            });
+    updatebang('defaultDurations', { 'default' : 200, plantFooded : 100 });
     return STATE;
 };
 function updateZombie(zombie) {
-    zombie.x -= 1;
+    zombie.x -= 0.1;
     return zombie;
 };
 function updateZombies() {
     return STATE.zombies = mapcar(updateZombie, STATE.zombies);
 };
-function playPause() {
-    STATE.running = !STATE.running;
+function pause() {
+    STATE.running = false;
     return mrender();
 };
-initStatebang();
-minBy = R.minBy;
-prop = R.prop;
-reduce = R.reduce;
-objValues = R.values;
+function playPause() {
+    return STATE.running ? pause() : (STATE.running = true);
+};
 function leftMost(zombies) {
-    return reduce(minBy(prop('x')), { 'x' : Infinity }, objValues(zombies));
+    var zombies230 = objValues(zombies);
+    return reduce(minBy(prop('x')), head(zombies230), tail(zombies230));
 };
 function calculateCollisions() {
     var frontZombie = leftMost(STATE.zombies);
-    return frontZombie && (STATE.frontZombie = frontZombie, (function () {
-        var _js22 = shots();
-        var _js24 = _js22.length;
-        var collect25 = [];
-        for (var _js23 = 0; _js23 < _js24; _js23 += 1) {
-            var aShot = _js22[_js23];
-            if (frontZombie.x <= aShot.x) {
-                collect25.push({ 'shot' : aShot, 'zombie' : frontZombie });
+    if (frontZombie) {
+        STATE.frontZombie = frontZombie;
+        return (function () {
+            var _js231 = shots();
+            var _js233 = _js231.length;
+            var collect234 = [];
+            for (var _js232 = 0; _js232 < _js233; _js232 += 1) {
+                var aShot = _js231[_js232];
+                if (frontZombie.x <= aShot.x) {
+                    collect234.push({ 'shot' : aShot, 'zombie' : frontZombie });
+                };
             };
-        };
-        return collect25;
-    })());
+            return collect234;
+        })();
+    } else {
+        return [];
+    };
 };
 function processCollisionbang(collision) {
     var zombieId = collision.zombie['index'];
@@ -132,29 +227,15 @@ function processCollisionsbang() {
 function shots() {
     return objValues(STATE.shots);
 };
-filter = R.filter;
 fitlerDead = filter(R.negate(prop('dead')));
-prop = R.prop;
 /** update all the things */
 function tickbang() {
     STATE.time = 1 + STATE.time;
     updateZombies();
-    STATE['shots'] = R.map(updateShot, STATE['shots']);
+    STATE['shots'] = mapcar(updateShot, STATE['shots']);
+    STATE['plants'] = mapcar(updatePlant, STATE['plants']);
     STATE['collisions'] = calculateCollisions();
     processCollisionsbang();
     STATE['shots'] = filter(alivep, STATE['shots']);
     return STATE['zombies'] = filter(alivep, STATE['zombies']);
-};
-tickbang();
-function mainLoop() {
-    try {
-        return STATE.running && (tickbang(), 'undefined' !== typeof render && render(), 'undefined' !== typeof mrender && mrender());
-    } finally {
-        requestAnimationFrame(mainLoop);
-    };
-};
-mainLoop();
-function step() {
-    tickbang();
-    return mrender();
 };
